e2e8bf9afcbfe8bb410d074d38776701efe7e5dc
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-08-17 23:37:53 +0330

    change base url to default

--- next.config.js
+++ next.config.js
@@ -1,40 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/nextjs',
-  reactStrictMode: true,
-  "typescript": {
-    "ignoreBuildErrors": true,
-  },
-    assetPrefix: '/nextjs',
-    rewrites() {
-      return [
-        { source: '/nextjs/_next/:path*', destination: '/nextjs/_next/:path*' }
-      ]
-    },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.rgb.irpsc.com",
-      },
-      {
-        protocol: "https",
-        hostname: "admin.rgb.irpsc.com",
-      },
-      {
-        protocol: "https",
-        hostname: "dl.qzparadise.ir"
-      },
-      {
-        protocol: "https",
-        hostname: "irpsc.com"
-      },
-      {
-        protocol: "https",
-        hostname: "dl.qzparadise.ir"
-      }
-    ]
-  },
-  
-}
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = nextConfig;
