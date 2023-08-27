79a7e5073d59a07f761cec73480b33f111cbe753
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-08-27 10:00:35 +0330

    remove old next config & add new

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
+  images: {
+    domains: ["dl.qzparadise.ir", "api.rgb.irpsc.com"],
+  },
+  experimental: { esmExternals: true },
+};
