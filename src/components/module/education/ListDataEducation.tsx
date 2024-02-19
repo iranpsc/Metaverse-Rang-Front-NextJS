4f91bc9ce337b86b15fde010241eb9491380f1a3
ehsan markazi <ehsanmarkazi73@gmail.com>
2024-02-19 09:51:02 +0330

    update

--- src/components/module/education/ListDataEducation.tsx
+++ src/components/module/education/ListDataEducation.tsx
@@ -38,8 +38,8 @@ export default function ListDataEducation({ data }: any) {
                 priority={true}
                 className=" w-full h-full hover:blur-none transition-all duration-150 ease-in-out rounded-t-[10px]  object-cover"
               />
-              <div className="w-full h-full backdrop-blur-[3px] bg-black/20 hover:backdrop-blur-none xs:backdrop-blur-none absolute z-0 top-0">
-                <Video className="w-[78px] h-[78px] p-3 fill-blueLink dark:fill-dark-yellow absolute z-0 top-[100px] right-[43%] rounded-full bg-white/80" />
+              <div className="w-full h-full backdrop-blur-[3px] bg-black/20 hover:backdrop-blur-none xs:backdrop-blur-none absolute z-0 top-0 flex justify-center items-center">
+                <Video className="w-[78px] h-[78px] p-3 fill-blueLink dark:fill-dark-yellow  rounded-full bg-white/80" />
               </div>
             </div>
 
