66372ebd28964158206ab3553fb10e073ce0b8bf
ehsan markazi <ehsanmarkazi73@gmail.com>
2024-02-20 13:49:05 +0330

    update1000

---
+++ src/pages/[lang]/citizen/test/index.tsx
@@ -0,0 +1,50 @@
+export default function Citizen() {
+  return (
+    <div className="grid grid-cols-12 gap-1 overflow-clip">
+      <div className="col-span-3 bg-white h-screen flex flex-col pb-1 justify-between items-center gap-2">
+        <div className="bg-black/50 w-full h-full">Header</div>
+        <div className=" w-full flex flex-col justify-between items-center gap-2">
+          <div className=" min-h-[200px]  bg-blueLink w-full">Title</div>
+          <div className="min-h-[100px] bg-error w-full">Title</div>
+          <div className="h-[400px] bg-mediumGray w-full">Title</div>
+        </div>
+        {/* FOOTER */}
+        <div className="flex flex-row justify-evenly items-start gap-5  bg-gray/90 w-full py-1">
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+        </div>
+      </div>
+
+      <div className="col-span-4 bg-white h-screen flex flex-col pb-1 justify-between items-center gap-2">
+        <div className="bg-black/50 w-full h-full">Header</div>
+        <div className=" w-full flex flex-col justify-between items-center gap-2">
+          <div className=" min-h-[200px]  bg-blueLink w-full">Title</div>
+          <div className="min-h-[100px] bg-error w-full">Title</div>
+          <div className="h-[400px] bg-mediumGray w-full">Title</div>
+        </div>
+        {/* FOOTER */}
+        <div className="flex flex-row justify-evenly items-start gap-5  bg-gray/90 w-full py-1">
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+        </div>
+      </div>
+
+      <div className="col-span-5 bg-white h-screen flex flex-col pb-1 justify-between items-center gap-2">
+        <div className="bg-black/50 w-full h-full">Header</div>
+        <div className=" w-full flex flex-col justify-between items-center gap-2">
+          <div className=" min-h-[200px]  bg-blueLink w-full">Title</div>
+          <div className="min-h-[100px] bg-error w-full">Title</div>
+          <div className="h-[400px] bg-mediumGray w-full">Title</div>
+        </div>
+        {/* FOOTER */}
+        <div className="flex flex-row justify-evenly items-start gap-5  bg-gray/90 w-full py-1">
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+          <div className="w-20 h-20 bg-error rounded-xl"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
