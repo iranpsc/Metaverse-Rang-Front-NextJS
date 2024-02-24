dd4631b1d819e5da464278c7f13eb499553f6770
ehsan markazi <ehsanmarkazi73@gmail.com>
2024-02-24 12:35:22 +0330

    update

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -5,6 +5,11 @@ import { AnimatePresence } from "framer-motion";
 import axios from "axios";
 import MenuItemPage from "./templates/MenuItemPage";
 import { SideBarContext } from "./context/SidebarContext";
+import LoginMenuModule from "./module/menu/LoginMenuModule";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useToken } from "./context/TokenContext";
+import { ArrowMenu } from "./svgs";
 
 interface Props {
   children: ReactNode;
@@ -22,6 +27,8 @@ export default function BaseLayout({
 }: Props) {
   const [showAuthCard, setShowAuthCard] = useState<boolean>(false);
   const { state } = useContext(SideBarContext);
+  const { code, token } = useToken();
+  const router = useRouter();
 
   return (
     <div className=" flex flex-row max-h-screen max-lg:h-full  max-lg:flex-col xl:overflow-clip lg:overflow-clip md:overflow-auto sm:overflow-auto xs:overflow-auto no-scrollbar">
@@ -33,7 +40,7 @@ export default function BaseLayout({
           <MenuItemPage setShowAuthCard={setShowAuthCard} />
         )}
       </AnimatePresence>
-      <section>
+      <section className="relative flex flex-row">
         <SideBarEducation
           setShowAuthCard={setShowAuthCard}
           pageName="citizen"
@@ -41,6 +48,39 @@ export default function BaseLayout({
           titleData={titleData}
           setShowLogOut={setShowLogOut}
         />
+        <div className=" w-[200px] h-full absolute end-[-210px] z-[999] flex flex-col justify-between items-start">
+          <div className="rounded-full w-[35px] h-[35px] bg-gray mt-3 flex justify-center items-center">
+            <ArrowMenu className="stroke-dark-gray" />
+          </div>
+
+          <div className=" flex flex-col gap-2 w-[85%] pt-2 bg-blueLink mb-[15px] rounded-md px-1 ">
+            {token && !code && (
+              <Link href={`https://gmail.com`} target="_blank">
+                <p className="text-white dark:text-black text-[14px] hover:text-[15px] font-azarMehr font-normal cursor-pointer">
+                  فعال سازی حساب کاربری
+                </p>
+                <hr className=" text-white dark:text-[#2D2D2A38] mt-1" />
+              </Link>
+            )}
+
+            <a href="https://rgb.irpsc.com/">
+              <p className="text-white dark:text-black text-[14px] hover:text-[15px] font-azarMehr font-normal cursor-pointer xs:text-[12px] whitespace-nowrap">
+                {state.dataLogin[3] && state.dataLogin[3].translation}
+              </p>
+            </a>
+            <hr className=" text-white dark:text-[#2D2D2A38]" />
+            <a href="https://rgb.irpsc.com/metaverse">
+              <p className="text-white dark:text-black text-[14px]  hover:text-[15px] font-azarMehr font-normal  cursor-pointer xs:text-[12px] whitespace-nowrap">
+                {state.dataLogin[2] && state.dataLogin[2].translation}
+              </p>
+            </a>
+            <hr className=" text-white dark:text-[#2D2D2A38]" />
+            <p className="text-white dark:text-black text-[14px]  hover:text-[15px] font-azarMehr font-normal cursor-pointer xs:text-[12px] whitespace-nowrap">
+              {state.dataLogin[1] && state.dataLogin[1].translation}
+            </p>
+            <hr className=" text-white dark:text-[#2D2D2A38]" />
+          </div>
+        </div>
       </section>
       {children}
     </div>
--- src/components/module/education/ListDataEducation.tsx
+++ src/components/module/education/ListDataEducation.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import {
-  Like,
-  Dislike,
-  View,
-  Video,
-  Videos,
-} from "@/components/svgs/SvgEducation";
+import { Like, Dislike, View, Video } from "@/components/svgs/SvgEducation";
 import { formatNumber } from "@/components/utils/education";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function ListDataEducation({ data }: any) {
   const router = useRouter();
   const { lang } = router.query;
+  const { theme } = useTheme();
 
   const pusher = (data: any) => {
     router.push(`/${lang}/education/category/${data}`);
   };
 
   const pushRgb = (data: any) => {
-    router.push(`https://rgb.irpsc.com/${lang}/citizen/${data}`);
+    // // router.push(`https://rgb.irpsc.com/${lang}/citizen/${data}`);
+    // <Link href="https://rgb.irpsc.com/${lang}/citizen/${data}" target="_blank">
+    //   https://rgb.irpsc.com/${lang}/citizen/${data}
+    // </Link>;
   };
 
   return (
@@ -43,7 +44,7 @@ export default function ListDataEducation({ data }: any) {
               </div>
             </div>
 
-            <div className="flex flex-row justify-start items-center gap-1 w-[95%] mt-[-10px] pe-20">
+            <div className="flex flex-row justify-start items-center gap-1 w-full mt-[-10px] pe-16">
               <p
                 className="text-start text-gray dark:text-dark-gray font-medium font-azarMehr text-[16px]  3xl:text-[18px] cursor-pointer hover:text-blueLink hover:dark:text-dark-yellow"
                 onClick={() => pusher(item.category.slug)}
@@ -55,11 +56,24 @@ export default function ListDataEducation({ data }: any) {
               </span>
 
               <p
-                className=" text-start  text-gray dark:text-dark-gray truncate font-medium font-azarMehr text-[16px] 3xl:text-[18px] cursor-pointer hover:text-blueLink hover:dark:text-dark-yellow"
+                className=" text-start  text-gray dark:text-dark-gray  whitespace-nowrap font-medium font-azarMehr text-[16px] 3xl:text-[18px] cursor-pointer hover:text-blueLink hover:dark:text-dark-yellow"
                 onClick={() => pusher(item.category.slug)}
+                data-tooltip-id={item.sub_category.name}
               >
-                {item.sub_category.name}
+                {item.sub_category.name.length > 30
+                  ? item.sub_category.name.slice(0, 25) + "..."
+                  : item.sub_category.name}
               </p>
+              <ReactTooltip
+                id={item.sub_category.name}
+                place="bottom"
+                content={item.sub_category.name}
+                style={{
+                  backgroundColor: "#737272",
+                  color: "#fff",
+                  fontWeight: "bold",
+                }}
+              />
             </div>
 
             <h1 className="text-start w-[95%] font-azarMehr truncate cursor-pointer font-bold mt-[15px] text-[18px] 3xl:text-[22px] ">
@@ -68,24 +82,28 @@ export default function ListDataEducation({ data }: any) {
             <div className="flex flex-row items-center justify-start  mt-[-8px] w-[98%]"></div>
             <div className="w-[95%] pb-2 flex flex-row justify-between  items-center">
               <>
-                <div className="flex flex-row justify-start items-center gap-2">
-                  <Image
-                    src={item.creator.image}
-                    alt={item.creator.code}
-                    width={1000}
-                    height={1000}
-                    loading="lazy"
-                    className="w-[45px] h-[45px] rounded-full object-cover cursor-pointer transition-all duration-150 ease-in-out"
-                    onClick={() => pushRgb(item.creator.code)}
-                  />
-                  <span
-                    className="text-blueLink  cursor-pointer text-[16px] 3xl:text-[22px] whitespace-nowrap font-medium hover:font-bold uppercase "
-                    onClick={() => pushRgb(item.creator.code)}
-                  >
-                    {item.creator.code}
-                  </span>
-                </div>
-
+                <Link
+                  href={`https://rgb.irpsc.com/${lang}/citizen/${item.creator.code}`}
+                  target="_blank"
+                >
+                  <div className="flex flex-row justify-start items-center gap-2">
+                    <Image
+                      src={item.creator.image}
+                      alt={item.creator.code}
+                      width={1000}
+                      height={1000}
+                      loading="lazy"
+                      className="w-[45px] h-[45px] rounded-full object-cover cursor-pointer transition-all duration-150 ease-in-out"
+                      // onClick={() => pushRgb(item.creator.code)}
+                    />
+                    <span
+                      className="text-blueLink  cursor-pointer text-[16px] 3xl:text-[22px] whitespace-nowrap font-medium hover:font-bold uppercase "
+                      // onClick={() => pushRgb(item.creator.code)}
+                    >
+                      {item.creator.code}
+                    </span>
+                  </div>
+                </Link>
                 <div className="flex flex-row justify-start items-center gap-5">
                   <span className=" whitespace-nowrap font-azarMehr font-normal 3xl:text-[18px] text-gray dark:text-dark-gray me-[-13px]">
                     {formatNumber(item.dislikes_count)}
