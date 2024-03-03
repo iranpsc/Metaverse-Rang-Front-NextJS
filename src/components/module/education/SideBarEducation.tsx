78082bbe78860d9a59f291bdbd43232fa7cd62e7
ehsan markazi <ehsanmarkazi73@gmail.com>
2024-03-03 18:42:15 +0330

    update

--- src/components/module/education/SideBarEducation.tsx
+++ src/components/module/education/SideBarEducation.tsx
@@ -124,7 +124,9 @@ export default function SideBarEducation({
   return (
     <div className="   xl:relative lg:relative   dark:bg-dark-background  overflow-y-clip">
       <h1>{languageSelected.file_url}</h1>
-      <h1>{testy}</h1>
+      <h1>{testy.code}</h1>
+      <h1>{testy.message}</h1>
+      <h1>{testy.name}</h1>
       <div
         className={` shadow-left dark:shadow-leftDark xl:min-h-screen scroll lg:min-h-screen md:min-h-screen overflow-y-scroll  relative sm:min-h-screen xs:min-h-screen ${
           state.isCollapsed
