4469cd2f471fbfd21790cd414a8c2fc16acd1885
unknown <mohseni.amir.eh@gmail.com>
2024-08-06 01:18:51 -0700

    new TAILWIND config implemented in project, but still we had some old config which is added to config

--- src/app/[lang]/levels/page.tsx
+++ src/app/[lang]/levels/page.tsx
@@ -1,5 +1,9 @@
-
-import { getAllLevels , getFooterData , getTransletion , getMainFile, } from "@/components/utils/actions";
+import {
+  getAllLevels,
+  getFooterData,
+  getTransletion,
+  getMainFile,
+} from "@/components/utils/actions";
 import DynamicFooter from "@/components/module/footer/DynamicFooter";
 import LevelCard from "@/components/module/levelComponent/LevelCard";
 
@@ -8,31 +12,30 @@ export default async function LevelsPage({
 }: {
   params: { lang: "en" | "fa" };
 }) {
+  const levelArray = await getAllLevels();
+  const footerTabs = await getFooterData(params);
 
-  const levelArray = await getAllLevels()
-  const footerTabs = await getFooterData(params)
-  
   const langData = await getTransletion(params.lang);
-  const mainData = await getMainFile(langData)
-  const levels = mainData.modals.find((x:any)  => x.name == "levels")
-  const levelsTranslatePage =levels.tabs.find((x:any)  => x.name == "levels-page").fields
+  const mainData = await getMainFile(langData);
+  const levels = mainData.modals.find((x: any) => x.name == "levels");
+  const levelsTranslatePage = levels.tabs.find(
+    (x: any) => x.name == "levels-page"
+  ).fields;
 
-
-  
   return (
     <>
-    <div className="flex justify-center flex-wrap">
-
-      {levelArray.data.map((item:any) => (
-        <LevelCard
-         item={item}
-         levelsTranslatePage={levelsTranslatePage}
-         params={params}/>
-      ))}
-    </div>
-       <div className="flex flex-col justify-center items-center">
-            <DynamicFooter  footerTabs={footerTabs}/>
-          </div>
+      <div className="flex justify-center flex-wrap">
+        {levelArray.data.map((item: any) => (
+          <LevelCard
+            item={item}
+            levelsTranslatePage={levelsTranslatePage}
+            params={params}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <DynamicFooter footerTabs={footerTabs} />
+      </div>
     </>
   );
 }
