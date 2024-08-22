6d3f48c9427015ff4516531c69441c63d8b6180b
warchief99 <hamid.keymasi1991@gmail.com>
2024-08-22 03:42:47 -0700

    edit features component

--- src/components/module/levelComponent/Features.tsx
+++ src/components/module/levelComponent/Features.tsx
@@ -9,7 +9,7 @@ export const Features = ({ levelsTranslatePage }: any) => {
         <div className="mb-[-65px] xl:mb-[-90px] 2xl:mb-[-100px]  w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] xl:w-[150px] xl:h-[150px] 2xl:w-[170px] 2xl:h-[170px] 3xl:w-[180px] 3xl:h-[180px]  box-content  p-4 text-center dark:bg-darkGray_1 bg-bgLightGrey rounded-full z-[1]">
           <Income className="w-full h-full" />
         </div>
-        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15px] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px]  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
+        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15%] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px] lg:h-[210px] xl:h-[260px] 2xl:h-[280px] 2xl  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
           <span className="text-xl xl:text-2xl font-bold py-2">
             {targetData(levelsTranslatePage, "income")}
           </span>
@@ -26,7 +26,7 @@ export const Features = ({ levelsTranslatePage }: any) => {
         <div className="mb-[-65px] xl:mb-[-90px] 2xl:mb-[-100px]  w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] xl:w-[150px] xl:h-[150px] 2xl:w-[170px] 2xl:h-[170px] 3xl:w-[180px] 3xl:h-[180px] box-content p-4 text-center dark:bg-darkGray_1 bg-bgLightGrey rounded-full z-[1]">
           <Orders className="w-full h-full" />
         </div>
-        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15px] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px]  flex flex-col justify-center items-center  dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
+        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15%] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px] lg:h-[210px] xl:h-[260px] 2xl:h-[280px] 2xl  flex flex-col justify-center items-center  dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
           <span className="text-xl xl:text-2xl font-bold py-2">
             {targetData(levelsTranslatePage, "orders")}
           </span>
@@ -42,7 +42,7 @@ export const Features = ({ levelsTranslatePage }: any) => {
         <div className="mb-[-65px] xl:mb-[-90px] 2xl:mb-[-100px]  w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] xl:w-[150px] xl:h-[150px] 2xl:w-[170px] 2xl:h-[170px] 3xl:w-[180px] 3xl:h-[180px]  box-content  p-4 text-center dark:bg-darkGray_1 bg-bgLightGrey rounded-full z-[1]">
           <Development className="w-full h-full" />
         </div>
-        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15px] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px]  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white   base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
+        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15%] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px] lg:h-[210px] xl:h-[260px] 2xl:h-[280px] 2xl  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white   base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
           <span className="text-xl xl:text-2xl font-bold py-2">
             {targetData(levelsTranslatePage, "development")}
           </span>
@@ -58,7 +58,7 @@ export const Features = ({ levelsTranslatePage }: any) => {
         <div className="mb-[-65px] xl:mb-[-90px] 2xl:mb-[-100px]  w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] xl:w-[150px] xl:h-[150px] 2xl:w-[170px] 2xl:h-[170px] 3xl:w-[180px] 3xl:h-[180px] box-content  p-4 text-center dark:bg-darkGray_1 bg-bgLightGrey rounded-full z-[1]">
           <Update className="w-full h-full" />
         </div>
-        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15px] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px]  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
+        <div className="w-full rounded-[20px] cursor-cell p-3 pb-[15%] pt-[95px] xl:pt-[120px] 2xl:pt-[130px] 3xl:pt-[150px] h-[195px] lg:h-[210px] xl:h-[260px] 2xl:h-[280px] 2xl  flex flex-col  justify-center items-center dark:bg-[#080807] bg-white base-shadow-1 hover:border hover:border-blueLink hover:dark:border-dark-yellow">
           <span className="text-xl xl:text-2xl font-bold py-2">
             {targetData(levelsTranslatePage, "update")}
           </span>
