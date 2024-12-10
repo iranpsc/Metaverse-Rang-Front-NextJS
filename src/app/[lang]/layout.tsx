81743ea79a17d451f1cd9395a5dff3b65214c8c2
unknown <mohseni.amir.eh@gmail.com>
2024-12-10 00:18:06 -0800

    fix:pre-loading some data to better load

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -9,6 +9,18 @@ export default function LangLayout({ children, params }: any) {
 
   return (
     <html className={theme} lang={params.lang}>
+      <head>
+        {/* Preload the poster image */}
+        <link rel="preload" as="image" href="/firstpage/replaced_pic.png" />
+
+        {/* Preload the video */}
+        <link
+          rel="preload"
+          as="video"
+          href="/firstpage/3d_rgb.irpsc.mp4"
+          type="video/mp4"
+        />
+      </head>
       <body
         className={`${azarMehr.variable} ${rokh.variable} h-screen light-scrollbar dark:dark-scrollbar`}
       >
