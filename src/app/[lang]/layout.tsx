e91ec010b8bd4ebb4577a1c21da86e1e1d7fabdb
unknown <mohseni.amir.eh@gmail.com>
2024-12-10 00:01:55 -0800

    move html to /lang layout to dynamically handle lang={params.lang}

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,24 @@
+import { azarMehr } from "@/components/utils/fonts";
+import { rokh } from "@/components/utils/fonts";
+import useServerDarkMode from "src/hooks/use-server-dark-mode";
+import ToastProvider from "../../components/shared/toastProvider";
+import { Suspense } from "react";
+
 export default function LangLayout({ children, params }: any) {
+  const theme = useServerDarkMode();
+
   return (
-    <html lang={params.lang}>
-      <body>{children}</body>
+    <html className={theme} lang={params.lang}>
+      <body
+        className={`${azarMehr.variable} ${rokh.variable} h-screen light-scrollbar dark:dark-scrollbar`}
+      >
+        <ToastProvider />
+        <Suspense
+          fallback={<div className="text-center text-[20px]">Loading...</div>}
+        >
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import "./../styles/global.css";
-import { azarMehr } from "@/components/utils/fonts";
-import { rokh } from "@/components/utils/fonts";
-import useServerDarkMode from "src/hooks/use-server-dark-mode";
-import ToastProvider from '../components/shared/toastProvider';
-import { Suspense } from 'react';
+// import { azarMehr } from "@/components/utils/fonts";
+// import { rokh } from "@/components/utils/fonts";
+// import useServerDarkMode from "src/hooks/use-server-dark-mode";
+// import ToastProvider from '../components/shared/toastProvider';
+// import { Suspense } from 'react';
 
 
 export const metadata = {
@@ -25,15 +25,17 @@ export default function RootLayout({
 //   params: { lang: any };
 // }
 ) {
-  const theme = useServerDarkMode();
+  // const theme = useServerDarkMode();
   return (
-    <html className={theme}>
-      <body className={`${azarMehr.variable} ${rokh.variable} h-screen light-scrollbar dark:dark-scrollbar`}>
-      <ToastProvider />
-        <Suspense fallback={<div className="text-center text-[20px]">Loading...</div>}>
+    // <html className={theme}>
+    //   <body className={`${azarMehr.variable} ${rokh.variable} h-screen light-scrollbar dark:dark-scrollbar`}>
+    <>
+      {/* <ToastProvider /> */}
+        {/* <Suspense fallback={<div className="text-center text-[20px]">Loading...</div>}> */}
           {children}
-        </Suspense>
-      </body>
-    </html>
+        {/* </Suspense> */}
+    </>
+    //   </body>
+    // </html>
   );
 }
