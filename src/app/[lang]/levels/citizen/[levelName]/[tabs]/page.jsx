c98b19a5e3dd9a65ac8783d225ac7a50f092b32f
unknown <mohseni.amir.eh@gmail.com>
2025-01-02 07:43:34 -0800

    fix:improving script to reduce load delay, push to test on server

--- src/app/[lang]/levels/citizen/[levelName]/[tabs]/page.jsx
+++ src/app/[lang]/levels/citizen/[levelName]/[tabs]/page.jsx
@@ -126,10 +126,37 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function lavelSingelPage({ params }) {
+  const staticRouteNames = [
+    { id: 1, route_name: "citizen-baguette" },
+    { id: 2, route_name: "reporter-baguette" },
+    { id: 3, route_name: "participation-baguette" },
+    { id: 4, route_name: "developer-baguette" },
+    { id: 5, route_name: "inspector-baguette" },
+    { id: 6, route_name: "businessman-baguette" },
+    { id: 7, route_name: "lawyer-baguette" },
+    { id: 8, route_name: "city-council-baguette" },
+    { id: 9, route_name: "the-mayor-baguette" },
+    { id: 10, route_name: "governor-baguette" },
+    { id: 11, route_name: "minister-baguette" },
+    { id: 12, route_name: "judge-baguette" },
+    { id: 13, route_name: "legislator-baguette" },
+  ];
+
+  const levelId = staticRouteNames.find(x => x.route_name === params.levelName)?.id
 
   
-  const langData = await getTranslation(params.lang);
-  const footerTabs = await getFooterData(params);
+  // const langData = await getTranslation(params.lang);
+  // const footerTabs = await getFooterData(params);
+  // const singleLevel = await getSingleLevel(levelId);
+  // const levelTabs = await getLevelTabs(params, levelId);
+
+  const [langData, footerTabs, singleLevel, levelTabs] = await Promise.all([
+    getTranslation(params.lang),
+    getFooterData(params),
+    getSingleLevel(levelId),
+    getLevelTabs(params, levelId)
+  ]);
+
 
   const mainData = await getMainFile(langData);
   const levelsOld = mainData.modals.find((x) => x.name == "levels");
@@ -139,8 +166,13 @@ export default async function lavelSingelPage({ params }) {
 
   const levels = await findByModalName(mainData, "levels");
 
-  const levelPageArrayContent = await findByTabName(levels, "levels-page");
-  const levelListArrayContent = await findByTabName(levels, "level-list");
+  // const levelPageArrayContent = await findByTabName(levels, "levels-page");
+  // const levelListArrayContent = await findByTabName(levels, "level-list");
+
+  const [levelPageArrayContent, levelListArrayContent] = await Promise.all([
+    findByTabName(levels, "levels-page"),
+    findByTabName(levels, "level-list"),
+  ]);
 
   const concatArrayContent = levelPageArrayContent.concat(
     levelListArrayContent
@@ -160,28 +192,6 @@ export default async function lavelSingelPage({ params }) {
       (item) => Number(item.name) == Number(levelId)
     )?.translation;
   }
-  
-
-  const staticRouteNames = [
-    { id: 1, route_name: "citizen-baguette" },
-    { id: 2, route_name: "reporter-baguette" },
-    { id: 3, route_name: "participation-baguette" },
-    { id: 4, route_name: "developer-baguette" },
-    { id: 5, route_name: "inspector-baguette" },
-    { id: 6, route_name: "businessman-baguette" },
-    { id: 7, route_name: "lawyer-baguette" },
-    { id: 8, route_name: "city-council-baguette" },
-    { id: 9, route_name: "the-mayor-baguette" },
-    { id: 10, route_name: "governor-baguette" },
-    { id: 11, route_name: "minister-baguette" },
-    { id: 12, route_name: "judge-baguette" },
-    { id: 13, route_name: "legislator-baguette" },
-  ];
-  const levelId = staticRouteNames.find(x => x.route_name === params.levelName)?.id
-
-  const singleLevel = await getSingleLevel(levelId);
-
-  const levelTabs = await getLevelTabs(params, levelId);
 
   // another schema for this page are on this page's components(tabs)
   const secondSchema = {
