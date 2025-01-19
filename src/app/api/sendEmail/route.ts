8665a72b7e87f2445ed2e06d7cf4a8308bc3c5e8
unknown <mohseni.amir.eh@gmail.com>
2025-01-18 22:51:38 -0800

    fix:test SMTP on server

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 export async function POST(req: NextRequest) {
+  console.log("SMTP_HOST:", process.env.SMTP_HOST);
+  console.log("SMTP_PORT:", process.env.SMTP_PORT);
+  console.log("SMTP_USER:", process.env.SMTP_USER);
+  console.log("SMTP_PASS:", process.env.SMTP_PASS);
   try {
     const { to, cc, bcc, message } = await req.json();
 
