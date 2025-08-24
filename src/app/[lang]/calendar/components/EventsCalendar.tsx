c8ff18f28230492f54bfa19ebe16700d46fd12c2
parsa <154906383+parsabr80@users.noreply.github.com>
2025-08-24 09:33:54 +0330

    fix bugs

--- src/app/[lang]/calendar/components/EventsCalendar.tsx
+++ src/app/[lang]/calendar/components/EventsCalendar.tsx
@@ -13,7 +13,6 @@ export default function EventsCalendar({
   params,
   token,
 }: CalendarProps) {
-  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
   const [searchResults, setSearchResults] = useState<MappedEventItem[] | null>(
     null
@@ -21,8 +20,8 @@ export default function EventsCalendar({
   const [startOfMonthDate, SetStartOfMonthDate] = useState<string>("");
   const [endOfMonthDate, setEndOfMonthDate] = useState<string>("");
   const [eventsDay, setEventsDay] = useState<any[]>([]);
-    const [MonthEventsDay, setMonthEventsDay] = useState<any[]>([]);
-   console.log("test is",MonthEventsDay)
+  const [MonthEventsDay, setMonthEventsDay] = useState<any[]>([]);
+  //console.log("test is",MonthEventsDay)
 
   const [selectedEventDate, setSelectedEventDate] = useState<string | null>(
     null
@@ -31,22 +30,32 @@ export default function EventsCalendar({
     null
   );
 
-const total = MonthEventsDay.length;
+  const total = MonthEventsDay.length;
 
-const grouped = MonthEventsDay.reduce((acc, event) => {
-  acc[event.color] = (acc[event.color] || 0) + 1;
-  return acc;
-}, {});
+  const grouped = MonthEventsDay.reduce((acc, event) => {
+    acc[event.color] = (acc[event.color] || 0) + 1;
+    return acc;
+  }, {});
 
-const bluePercent = grouped["#0000ff"] ? Math.round((grouped["#0000ff"] / total) * 100) : 0;
-const redPercent = grouped["#ff0000"] ? Math.round((grouped["#ff0000"] / total) * 100) : 0;
-const yellowPercent = grouped["#ffff00"] ? Math.round((grouped["#ffff00"] / total) * 100) : 0;
-const greenPercent = grouped["#32DA6B"] ? Math.round((grouped["#80ff00"] / total) * 100) : 0;
-const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total) * 100) : 0;
+  const bluePercent = grouped["#0000ff"]
+    ? Math.round((grouped["#0000ff"] / total) * 100)
+    : 0;
+  const redPercent = grouped["#ff0000"]
+    ? Math.round((grouped["#ff0000"] / total) * 100)
+    : 0;
+  const yellowPercent = grouped["#ffff00"]
+    ? Math.round((grouped["#ffff00"] / total) * 100)
+    : 0;
+  const greenPercent = grouped["#32DA6B"]
+    ? Math.round((grouped["#80ff00"] / total) * 100)
+    : 0;
+  const pinkPercent = grouped["#ff00ff"]
+    ? Math.round((grouped["#ff00ff"] / total) * 100)
+    : 0;
 
   useEffect(() => {
     if (searchValue.trim() === "") {
-      setSearchResults(null); 
+      setSearchResults(null);
     }
   }, [searchValue]);
 
@@ -60,7 +69,7 @@ const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total)
         const json = await res.json();
         const newEvents = json.data;
         setEventsDay((prev) => [...prev, ...newEvents]);
-        setMonthEventsDay(newEvents)
+        setMonthEventsDay(newEvents);
       } catch (error) {
         console.error(error);
       }
@@ -112,25 +121,6 @@ const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total)
       });
   }
 
-  function toggleFilter(color: string) {
-    setSelectedFilters((prev) => {
-      let updated: string[];
-      if (prev.includes(color)) {
-        updated = prev.filter((c) => c !== color);
-      } else {
-        updated = [...prev, color];
-      }
-      if (color == "all") {
-        updated = [];
-      }
-      return updated;
-    });
-  }
-  function getFilterClasses(color: string) {
-    const isSelected = selectedFilters.includes(color);
-    const activeClasses = "bg-[#F8F8F8] dark:bg-black";
-    return isSelected ? activeClasses : null;
-  }
   return (
     <div className="centerItem w-[95%] lg:w-full  pt-6 text-black dark:text-white bg-white  dark:bg-[#080807] flex flex-col items-center rounded-[20px] gap-2 font-['Montserrat']  ">
       <div className="w-[97%]  flex flex-col items-start sm:flex-row-reverse lg:w-[95%] lg:gap-4 ">
@@ -177,11 +167,10 @@ const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total)
             </h2>
 
             <div
-              onClick={() => {
-                toggleFilter("all");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 px-2 w-[96%] hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11 lg:mt-2  
-            ${getFilterClasses("all")}`}
+              className="flex items-center justify-between h-11 2xl:h-12 px-2 w-[96%]
+                  rounded-md  item sm:h-[34px] 
+                 sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11 lg:mt-2  
+            "
             >
               <div className="flex items-center  h-7  xl:h-9 2xl:h-10">
                 <div className="whitespace-nowrap inline-block bg-transparent relative rounded-lg aspect-square h-full overflow-hidden  ">
@@ -197,105 +186,76 @@ const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total)
               </div>
             </div>
 
-            <div
-              onClick={() => {
-                toggleFilter("blue");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 px-2 w-[96%] hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl  xl:h-11
-          ${getFilterClasses("blue")}`}
-            >
-              <div className="flex items-center h-full  ">
-                <div className="bg-[#0000ff] rounded-lg aspect-square h-7  xl:h-9 2xl:h-10 "></div>
-                <span className="mx-2 whitespace-nowrap">
-                  {findByUniqueId(mainData, 578)}
-                </span>
+            {bluePercent !== 0 && (
+              <div className="flex items-center justify-between h-11 2xl:h-12 px-2 w-[96%]  rounded-md item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11">
+                <div className="flex items-center h-full">
+                  <div className="bg-[#0000ff] rounded-lg aspect-square h-7 xl:h-9 2xl:h-10"></div>
+                  <span className="mx-2 whitespace-nowrap">
+                    {findByUniqueId(mainData, 578)}
+                  </span>
+                </div>
+                <div className="text-[#0000ff]">
+                  {switchDigits(bluePercent, params.lang)} %
+                </div>
               </div>
-              <div className="text-[#0000ff] ">
-                {" "}
-                {switchDigits(bluePercent, params.lang)} %
-              </div>
-            </div>
+            )}
 
-            <div
-              onClick={() => {
-                toggleFilter("red");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11
-            
-                        ${getFilterClasses("red")}`}
-            >
-              <div className="flex items-center h-full">
-                <div className="bg-[#ff0000] rounded-lg aspect-square  xl:h-9  2xl:h-10 h-7 "></div>
-                <span className="mx-2 whitespace-nowrap">
-                  {" "}
-                  {findByUniqueId(mainData, 577)}
-                </span>
+            {redPercent !== 0 && (
+              <div className="flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 rounded-md item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11">
+                <div className="flex items-center h-full">
+                  <div className="bg-[#ff0000] rounded-lg aspect-square h-7 xl:h-9 2xl:h-10"></div>
+                  <span className="mx-2 whitespace-nowrap">
+                    {findByUniqueId(mainData, 577)}
+                  </span>
+                </div>
+                <div className="text-[#ff0000]">
+                  {switchDigits(redPercent, params.lang)} %
+                </div>
               </div>
-              <div className="text-[#ff0000]">
-                {" "}
-                {switchDigits(redPercent, params.lang)} %
-              </div>
-            </div>
+            )}
 
-            <div
-              onClick={() => {
-                toggleFilter("yellow");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11
-         ${getFilterClasses("yellow")} `}
-            >
-              <div className="flex items-center h-full ">
-                <div className="bg-[#ffff00] rounded-lg aspect-square   xl:h-9 2xl:h-10 h-7 "></div>
-                <span className="mx-2 whitespace-nowrap ">
-                  {findByUniqueId(mainData, 579)}
-                </span>
+            {yellowPercent !== 0 && (
+              <div className="flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 rounded-md item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11">
+                <div className="flex items-center h-full">
+                  <div className="bg-[#ffff00] rounded-lg aspect-square h-7 xl:h-9 2xl:h-10"></div>
+                  <span className="mx-2 whitespace-nowrap">
+                    {findByUniqueId(mainData, 579)}
+                  </span>
+                </div>
+                <div className="text-[#ffff00]">
+                  {switchDigits(yellowPercent, params.lang)} %
+                </div>
               </div>
-              <div className="text-[#ffff00] ">
-                {" "}
-                {switchDigits(yellowPercent, params.lang)} %
-              </div>
-            </div>
+            )}
 
-            <div
-              onClick={() => {
-                toggleFilter("green");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11
-          
-          ${getFilterClasses("green")}
-          `}
-            >
-              <div className="flex items-center h-full ">
-                <div className="bg-[#80ff00] rounded-lg aspect-square  xl:h-9  2xl:h-10 h-7 "></div>
-                <span className="mx-2 whitespace-nowrap">
-                  {findByUniqueId(mainData, 580)}
-                </span>
-              </div>
-              <div className="text-[#80ff00]">
-                {" "}
-                {switchDigits(greenPercent, params.lang)} %
-              </div>
-            </div>
+          {greenPercent !== 0 && (
+  <div className="flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 rounded-md item sm:h-[34px] sm:text-sm lg:text-base xl:text-lg 2xl:text-xl xl:h-11">
+    <div className="flex items-center h-full">
+      <div className="bg-[#80ff00] rounded-lg aspect-square h-7 xl:h-9 2xl:h-10"></div>
+      <span className="mx-2 whitespace-nowrap">
+        {findByUniqueId(mainData, 580)}
+      </span>
+    </div>
+    <div className="text-[#80ff00]">
+      {switchDigits(greenPercent, params.lang)} %
+    </div>
+  </div>
+)}
 
-            <div
-              onClick={() => {
-                toggleFilter("pink");
-              }}
-              className={`flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 hover:bg-[#F8F8F8] dark:hover:bg-black rounded-md cursor-pointer item sm:h-[34px] lg:text-base xl:text-lg 2xl:text-xl xl:h-11
-          ${getFilterClasses("pink")}
-          `}
-            >
-              <div className="flex items-center h-full ">
-                <div className="bg-[#ff00ff] rounded-lg aspect-square  xl:h-9  2xl:h-10 h-7 "></div>
-                <span className="mx-2 whitespace-nowrap">
-                  {findByUniqueId(mainData, 581)}
-                </span>
-              </div>
-              <div className="text-[#ff00ff]">
-                {" "}
-                {switchDigits(pinkPercent, params.lang)} %
-              </div>
-            </div>
+           {pinkPercent !== 0 && (
+  <div className="flex items-center justify-between h-11 2xl:h-12 w-[96%] px-2 rounded-md item sm:h-[34px] lg:text-base xl:text-lg 2xl:text-xl xl:h-11">
+    <div className="flex items-center h-full">
+      <div className="bg-[#ff00ff] rounded-lg aspect-square h-7 xl:h-9 2xl:h-10"></div>
+      <span className="mx-2 whitespace-nowrap">
+        {findByUniqueId(mainData, 581)}
+      </span>
+    </div>
+    <div className="text-[#ff00ff]">
+      {switchDigits(pinkPercent, params.lang)} %
+    </div>
+  </div>
+)}
+
           </div>
         </div>{" "}
       </div>
@@ -312,7 +272,6 @@ const pinkPercent = grouped["#ff00ff"] ? Math.round((grouped["#ff00ff"] / total)
         }
         mainData={mainData}
         params={params}
-        selectedFilters={selectedFilters}
       />
     </div>
   );
